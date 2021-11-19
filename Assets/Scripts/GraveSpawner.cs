b03619de5c6d0f036c36f69dcb14203e0d8a97ba
Joachim Schmidt <joachim.schmidt@wp.pl>
2021-11-19 17:11:26 +0100

    prepared generation of fence

--- Assets/Scripts/GraveSpawner.cs
+++ Assets/Scripts/GraveSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Models;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -8,6 +9,8 @@ using UnityEngine.Serialization;
 public class GraveSpawner : MonoBehaviour
 {
     public GameObject gravePrefab;
+    public GameObject fencePrefab;
+    public float scaleFactor;
 
     private void Start()
     {
@@ -15,16 +18,11 @@ public class GraveSpawner : MonoBehaviour
         Apply();
     }
 
-    private void Apply()
+    void spawnGraves(List<Grave> graves, Vector3 currentPosition)
     {
-        var cemetery = ApiConnection.GetCemetery(1);
-        List<Grave> graves = cemetery.tombstones;
-
-        var occupiedGraves = graves.Where(grave => grave.guest != null);
-        var currentPosition = transform.localPosition;
-        foreach (var grave in occupiedGraves)
+        foreach (var grave in graves)
         {
-            var gravePosition = new Vector3(4 * grave.gridX, -3, 4 * grave.gridY);
+            var gravePosition = new Vector3(scaleFactor * grave.gridX, 0, scaleFactor * grave.gridY);
             var finalPosition = gravePosition + currentPosition;
             GameObject gravePrefabInstance =
                 Instantiate(gravePrefab, finalPosition, Quaternion.identity, transform);
@@ -47,5 +45,29 @@ public class GraveSpawner : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void spawnFence(Cemetery cemetery, Vector3 currentPosition)
+    {
+        Vector3[] cornersOfCemetery =
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(scaleFactor * cemetery.maxGridX, 0, 0),
+            new Vector3(scaleFactor * cemetery.maxGridX, 0, scaleFactor * cemetery.maxGridY),
+            new Vector3(0, 0, scaleFactor * cemetery.maxGridY),
+        };
+        var fenceHorizontalScaleVector = new Vector3(scaleFactor * cemetery.maxGridX, 0, 0.4f);
+        var fenceVerticalScaleVector = new Vector3(scaleFactor * cemetery.maxGridY, 0, 0.4f);
+    }
+
+
+    private void Apply()
+    {
+        var cemetery = ApiConnection.GetCemetery(1);
+        List<Grave> graves = cemetery.tombstones;
+        var occupiedGraves = graves.Where(grave => grave.guest != null).ToList();
+        var currentPosition = transform.localPosition;
+        spawnGraves(occupiedGraves, currentPosition);
+        spawnFence(cemetery, currentPosition);
     }
 }
